5bef346f1fdcfd513a55be273fccd876cb66cabf
Akhil <achewadakhil11@gmail.com>
2025-07-26 20:34:00 +0530

    Added zod validators for signup and login routes

--- DevElevate/Server/routes/userRoutes.js
+++ DevElevate/Server/routes/userRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 const router = express.Router();
 
 import { registerUser, loginUser } from "../controller/userController.js";
+import { loginValidator, signUpValidator } from "../middleware/Validators.js";
 
-router.post("/signup", registerUser);
+router.post("/signup", signUpValidator, registerUser);
 
-router.post("/login", loginUser);
+router.post("/login", loginValidator, loginUser);
 
 export default router;
