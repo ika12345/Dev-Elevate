57e29f2aba2dce536a8675291c3de5b179397d91
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-08-04 23:43:46 +0530

    index.js

--- DevElevate/Server/index.js
+++ DevElevate/Server/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js";
 import cors from "cors"
 import userRoutes from './routes/userRoutes.js'
-import adminRoutes from './routes/adminRoutes.js'
+import adminRoutes from './Server/routes/adminRoutes.js';
 import cookieParser from "cookie-parser";
 import authorize from "./middleware/authorize.js";
 import { authenticateToken } from "./middleware/authMiddleware.js";
