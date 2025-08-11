599544938c633ab16120434830bc1d0065db52e4
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-08-11 22:27:01 +0530

    fixed delete usee

--- DevElevate/Client/src/config/routes.ts
+++ DevElevate/Client/src/config/routes.ts
@@ -4,7 +4,7 @@ export const baseUrl: string = import.meta.env.VITE_API_URL;
 export const adminApi = {
   allUser: "/api/v1/admin/all-users",
   addUser:"/api/v1/admin/add-user",
-  deleteUser:"/api/v1/admin/"
+  deleteUser:"/api/v1/admin/delete-user"
 
 
   
